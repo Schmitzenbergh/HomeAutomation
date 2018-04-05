e353024160a7a7a9f1d2b9dad4640c0e09be7bc5
dannyvanlierop <github@dannyvanlierop.nl>
2018-04-05 14:14:08 +0200

    ..

---
+++ src/_component/Loading JavaScript Modules with Component.js/script.sh
@@ -0,0 +1,12 @@
+$ component create my-components/my-randomer -l
+name: my-randomer
+description: random number generator
+does this component have js? y
+does this component have css? 
+does this component have html? 
+
+      create : my-components/my-randomer
+      create : my-components/my-randomer/index.js
+      create : my-components/my-randomer/Makefile
+      create : my-components/my-randomer/.gitignore
+      create : my-components/my-randomer/component.json
