7da86698cb251e01b3ed10bef2857ead3221044a
dannyvanlierop <github@dannyvanlierop.nl>
2018-04-17 17:05:08 +0200

    ..

--- default.js
+++ default.js
@@ -4,7 +4,7 @@ var hueStateNew = {};
 
 require('./config/var/src/db.paths');
 //module.exports.repl.ignoreUndefined = true;
-
+module.exports = VarHolder = {};
 exec = require('child_process').exec;
 fs = require('fs');
 net = require('net');
@@ -26,7 +26,7 @@ avr = require( pathavrhk171s + 'default.js');
 tv = require( pathsmarttv + 'tv.js' );
 server = require( pathserver + 'server.js' );
 
-
+test = require( pathlib + 'http.js' );
 
 /*******\
 | Start |###############################################################################################################################################################################
@@ -34,10 +34,23 @@ server = require( pathserver + 'server.js' );
 
 setTimeout(function(){ require('./server'); 
 
+  var options = {
+    hostname: '192.168.0.97',
+    port: 1925,
+    path: '/5/ambilight/processed',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/html',
+      'Content-Length': Buffer.byteLength("")
+    }
+  };
+
+
 //light.loadInfoAll();
 //sensor.loadInfoAll();
-
   setInterval(function(){  
+    console.log(counter++);
+    console.log(JSON.stringify(VarHolder));
     //light.getInfoAll();
     //sensor.getInfoAll();
 
@@ -45,7 +58,41 @@ setTimeout(function(){ require('./server');
     //  light.saveInfoAll();
     //  sensor.saveInfoAll();
     //}
-  }, 100);
+    //require( pathlib + 'http.js' );
+    //    test.httpGET('192.168.0.97', 1925, '/5/ambilight/processed');
+    //    console.log(" " + counter + test.httpGET('192.168.0.97', 1925, '/5/ambilight/processed'));
+
+  console.log(test.httpGET());
+  //console.log(test.httpGET());
+
+
+
+
+  }, 500);
 
 
 }, 3000);
+
+
+//var http = require("http");
+//var options = {
+//  hostname: '192.168.0.97',
+//  port: 1925,
+//  path: '/5/ambilight/processed',
+//  method: 'GET',
+//  headers: {
+//    'Content-Type': 'text/html',
+//    'Content-Length': Buffer.byteLength("")
+//  }
+//};
+//
+//var req = http.request(options, (res) => {
+//  res.on('data', (chunk) => { Result = "" + chunk;});
+//  //res.on('end', () => {});
+//});
+//
+////console.log(tempResult);
+//// write data to request body
+////req.write("abc");
+//req.end();
+
